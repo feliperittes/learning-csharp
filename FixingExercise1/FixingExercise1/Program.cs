f71ccd0330f246f4601169397ac8e0c5c172236f
feliperittes <feliperittes404@gmail.com>
2022-07-08 22:00:25 -0300

    Fixing Exercise

---
+++ FixingExercise1/FixingExercise1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FixingExercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Entre com seu nome completo: \n>");
+            string completeName = Console.ReadLine();
+
+            Console.Write("Quantos quartos tem na sua casa? \n>");
+            int roomQuantity = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o preço do produto: \n>");
+            double priceProduct = double.Parse(Console.ReadLine());
+
+            Console.Write("Entre com seu ultimo nome, idade e altura (mesma linha): \n>");
+            string[] array = Console.ReadLine().Split(' ');
+
+            string lastName = array[0];
+            int age = int.Parse(array[1]);
+            double height = double.Parse(array[2]);
+
+            Console.WriteLine("\n" + completeName);
+            Console.WriteLine(roomQuantity);
+            Console.WriteLine(priceProduct.ToString("c"));
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+
+            Console.ReadKey();
+        }
+    }
+}
