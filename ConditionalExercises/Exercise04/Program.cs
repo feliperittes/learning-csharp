0e5a7bc7dc9912ded50e7834aa9b280e47c9eb74
feliperittes <feliperittes404@gmail.com>
2022-07-11 19:36:51 -0300

    conditional structure

---
+++ ConditionalExercises/Exercise04/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ');
+            int beginningGame = int.Parse(array[0]);
+            int endGame = int.Parse(array[1]);
+
+            int duration;
+            if (beginningGame < endGame)
+            {
+                duration =  beginningGame - endGame;
+            }
+            else
+            {
+                duration = 24 - endGame + beginningGame;
+            }
+
+            Console.WriteLine($"O JOGO DUROU {Math.Abs(duration)} HORA(S)");
+
+            Console.ReadKey();
+        }
+    }
+}
