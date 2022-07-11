946dd3a7b3e152be3851fcebe3aab9a1404e342a
feliperittes <feliperittes404@gmail.com>
2022-07-11 18:00:56 -0300

    conditional structure

---
+++ ConditionalExercises/Exercise03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ');
+            int A = int.Parse(array[0]);
+            int B = int.Parse(array[1]);
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("SÂO MULTIPLOS");
+            }
+            else
+            {
+                Console.WriteLine("NÂO SÂO MULTIPLOS");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
