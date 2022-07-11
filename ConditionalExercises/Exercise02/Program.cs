12a1b2f7d56eb1d60b6567fb48c88d2e5b259fee
feliperittes <feliperittes404@gmail.com>
2022-07-11 17:48:35 -0300

    conditional structure

---
+++ ConditionalExercises/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NÃO NEGATIVO");
+            }
+            Console.ReadKey();
+        }
+    }
+}
