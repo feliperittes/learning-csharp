08ce4b4efdda1a00c1986c16b0f5ea9fe214b705
feliperittes <feliperittes404@gmail.com>
2022-07-11 17:44:53 -0300

    conditional structure

---
+++ ConditionalExercises/Exercise01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+                Console.ReadKey();
+        }
+    }
+}
