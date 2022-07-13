e0150bd4565e7109263d9c838b2ae15fd516113b
feliperittes <feliperittes404@gmail.com>
2022-07-13 17:29:05 -0300

    conditional structure

---
+++ ConditionalExercises/Exercise05/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercise05
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Digite digite o codigo do produto e a quantidade desejada(mesma linha separe com a tecla espaço):\n> ");
+            string[] array = Console.ReadLine().Split(' ');
+            int code = int.Parse(array[0]);
+            int quantity = int.Parse(array[1]);
+
+            if (code == 1)
+            {
+                double hotDog = 4.00;
+                Console.WriteLine($"Total a pagar: {(hotDog * quantity).ToString("C")}");
+            }
+            else if (code == 2)
+            {
+                double xSalad = 4.50;
+                Console.WriteLine($"Total a pagar: {(xSalad * quantity).ToString("C")}");
+            }else if (code == 3)
+            {
+                double xBacon = 5.00;
+                Console.WriteLine($"Total a pagar: {(xBacon * quantity).ToString("C")}");
+            }else if (code == 4)
+            {
+                double simpleToast = 2.00;
+                Console.WriteLine($"Total a pagar: {(simpleToast * quantity).ToString("C")}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
