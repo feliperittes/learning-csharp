82f244b7ac36dd39e2390f1110ce93e1fe12123e
feliperittes <feliperittes404@gmail.com>
2022-07-14 20:54:31 -0300

    while loop exercises

---
+++ WhileLoopExercises/Exercise01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string password = "2002";
+            Console.Write("Insira a senha correta: ");
+            string insertPassword = Console.ReadLine();
+
+            while (insertPassword != password)
+            {
+                Console.Write("\nAcesso Negado\nSenha invalida...");
+                Thread.Sleep(3000);
+                Console.Clear();
+
+                Console.Write("Insira a senha novamente: ");
+                insertPassword = Console.ReadLine();
+            }
+            Console.WriteLine("\nSenha correta!\nAcesso Permitido!");
+
+            Console.ReadKey();
+        }
+    }
+}
