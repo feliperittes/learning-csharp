4ac490968bd246a34dc9bd8dc0bf475c5ee036d0
feliperittes <feliperittes404@gmail.com>
2022-07-15 17:56:11 -0300

    for loop exercises

---
+++ ForLoopExercises/Exercise01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Insira um numero inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
