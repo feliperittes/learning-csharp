f666bc2198ef918cf4a9a55c576e901eccd2fdd8
feliperittes <feliperittes404@gmail.com>
2022-07-17 01:22:36 -0300

    conditional structure

--- ConditionalExercises/Exercise06/Program.cs
+++ ConditionalExercises/Exercise06/Program.cs
@@ -7,25 +7,25 @@
             Console.Write("Insira um valor: ");
             double number = double.Parse(Console.ReadLine());
 
-            if (number > 0 && number <= 25)
+            if (number < 0.0 || number > 100.0)
+            {
+                Console.WriteLine("Fora de intervalo");
+            }
+            else if (number <= 25.0)
             {
                 Console.WriteLine("Intervalo [0,25]");
             }
-            else if (number > 25 && number <= 50)
+            else if (number <= 50.0)
             {
                 Console.WriteLine("Intervalo [25,50]");
             }
-            else if (number > 50 && number <= 75)
+            else if (number <= 75.0)
             {
                 Console.WriteLine("Intervalo [50,75]");
             }
-            else if (number > 75 && number <= 100)
-            {
-                Console.WriteLine("Intervalo [75,100]");
-            }
             else
             {
-                Console.WriteLine("Fora de Intervalo");
+                Console.WriteLine("Intervalo [75,100]");
             }
 
             Console.ReadKey();
