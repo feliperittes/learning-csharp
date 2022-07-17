bd84896ab0d90435bd077ee2d9627c2d085cae88
feliperittes <feliperittes404@gmail.com>
2022-07-17 14:13:57 -0300

    conditional structure

---
+++ ConditionalExercises/Exercise07/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Exercise07
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Digite dois valores(mesma linha, usando espaço entre eles)\n" +
+                "que devem representar as coordenadas de um ponto em um plano:\n>");
+            string[] values = Console.ReadLine().Split(' ');
+            double X = double.Parse(values[0]);
+            double Y = double.Parse(values[1]);
+
+            if (X == 0.0 && Y == 0.0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (X == 0.0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            else if (Y == 0.0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if (X > 0.0 && Y > 0.0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (X < 0.0 && Y > 0.0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (X < 0.0 && Y < 0.0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else
+            {
+                Console.WriteLine("Q4");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConditionalExercises/Exercise08/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+namespace Exercise07
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double incomeTax;
+            Console.WriteLine("Insira o salário");
+            double salary = double.Parse(Console.ReadLine());
+            
+            if (salary <= 2000.0)
+            {
+                incomeTax = 0.0;
+            }
+            else if (salary <= 3000.0)
+            {
+                incomeTax = (salary - 2000.0) * 0.08;
+            }
+            else if (salary <= 4500.0)
+            {
+                incomeTax = (salary - 3000.0) * 0.18 + 1000.0 * 0.08;
+            }
+            else
+            {
+                incomeTax = (salary - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+            }
+
+            if (incomeTax == 0.0)
+            {
+                Console.WriteLine("ISENTO!");
+            }
+            else
+            {
+                Console.WriteLine($"{incomeTax.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
