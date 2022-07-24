955c42b0a9d0beb3162e79bfbe82b1bc738b54e9
feliperittes <feliperittes404@gmail.com>
2022-07-24 17:53:43 -0300

    for loop exercises

---
+++ ForLoopExercises/Exercise02/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercise02
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Insira a quantidade de numeros que serão lidos: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int count_in = 0;
+            int count_out = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write($"Insira o {i}º numero: ");
+                int x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20)
+                {
+                    count_in++;
+                }
+                else
+                {
+                    count_out++;
+                }
+            }
+            Console.WriteLine($"\n{count_in} numero(s) dentro.");
+            Console.WriteLine($"{count_out} numero(s) fora.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ForLoopExercises/Exercise03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise03
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Insira o numero casos de teste: ");
+            int N =  int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                string [] line = Console.ReadLine().Split(' ');
+                double a = double.Parse(line[0]);
+                double b = double.Parse(line[1]);
+                double c = double.Parse(line[2]);
+
+                double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+
+                Console.WriteLine(media.ToString("N1"));
+            }
+            Console.ReadKey();
+        }
+    }
+}
