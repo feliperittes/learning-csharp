4706abc3a7409d966e3c45a9e64ec3d8b52a11d1
feliperittes <feliperittes404@gmail.com>
2022-07-24 23:06:07 -0300

    for loop exercises

---
+++ ForLoopExercises/Exercise05/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise05
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Insira um valor para calcular sua faltorial: ");
+            int fatorial = int.Parse(Console.ReadLine());
+            int resultFatorial = 1;
+            for (int i = 1; i <= fatorial; i++)
+            {
+                resultFatorial *= i;
+            }
+            Console.WriteLine($"Fatorial escolhido {fatorial}! = {resultFatorial}.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ForLoopExercises/Exercise06/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise05
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Insira um valor para calculaR todos seus divisores: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = N; i >= 1; i--)
+            {
+                if (N % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
