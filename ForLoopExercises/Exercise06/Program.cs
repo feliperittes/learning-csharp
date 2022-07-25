b0706fa5faeb3c5cac735fa3fd60c27250dbff5b
feliperittes <feliperittes404@gmail.com>
2022-07-24 23:17:31 -0300

    for loop exercises

--- ForLoopExercises/Exercise06/Program.cs
+++ ForLoopExercises/Exercise06/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Exercise05
+﻿namespace Exercise06
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.Write("Insira um valor para calculaR todos seus divisores: ");
+            Console.Write("Insira um valor para calcular todos seus divisores: ");
             int N = int.Parse(Console.ReadLine());
 
             for (int i = N; i >= 1; i--)
---
+++ ForLoopExercises/Exercise07/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise05
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Insira um valor inteiro para quantidade de linhas: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"{i} {Math.Pow(i, 2)} {Math.Pow(i, 3)}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
